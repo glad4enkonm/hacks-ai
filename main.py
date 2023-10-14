95268f47e3d676dcb0145191c9f8c44b961adb9b
Konstantin Litvin <konstantinlitvin89@gmail.com>
2023-10-14 16:50:13 +0300

    Add flask app

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from collections import defaultdict
+from itertools import chain
 from pathlib import Path
 from typing import Dict
 
-from osmnx._errors import InsufficientResponseError
-import pandas as pd
-from districts import districts_spb
 import osmnx as ox
-import geopandas as gpd
-import matplotlib.pyplot as plt
+import pandas as pd
+from geopy.distance import geodesic
 
-from utils import read_yaml, to_pkl, load_pkl, project_root
+from utils import read_yaml, load_pkl, project_root
 
 ox.settings.use_cache = True
 
@@ -17,57 +14,6 @@ pd.set_option('display.max_columns', 25)
 pd.set_option('display.max_rows', 10000)
 pd.set_option('display.width', 1000)
 
-print(*districts_spb, sep='\n')
-
-city = "Санкт-Петербург, Russia"
-
-
-# def get_boundaries_of_a_city(city):
-#     city_gdf = ox.geocode_to_gdf(city)
-#     print(city_gdf['geometry'].iloc[0])
-
-
-def get_district_poly(district):
-    name = district + ' район, ' + city
-    print(name)
-    # graph = ox.graph_from_place(name, network_type="all")
-
-    boundary = ox.geocode_to_gdf(name)[["geometry", "name"]]
-
-    return boundary['geometry'].iloc[0]
-
-
-def get_amenities(polygon, tags: Dict):
-    try:
-        amenities = ox.features_from_polygon(polygon, tags=tags)
-        amenities.reset_index(inplace=True)
-        print(amenities.columns)
-        return amenities[['osmid', 'name', 'geometry', 'amenity']]
-    except InsufficientResponseError:
-        pass
-
-
-def dump_to_pkl():
-    """
-    dump data into data dir
-    """
-
-    # TODO: take into account difference between nodes and relations
-
-    dfs = {}
-    print(read_yaml('tags.yaml'))
-
-    tags = {'amenity': ['school', 'university', 'college', 'music_school', 'pub']}
-
-    for district in districts_spb[:3]:  # ! <- only 3
-        poly = get_district_poly(district)
-        print(poly)
-        df = get_amenities(poly, tags)
-        print(df)
-        dfs[district] = df
-
-        to_pkl(df, district)
-    # get_boundaries_of_a_city(city)
 
 
 def load_from_pkls():
@@ -77,8 +23,95 @@ def load_from_pkls():
     return dfs
 
 
-if __name__ == '__main__':
-    dump_to_pkl()  # first
+def get_tags(t_type):
+    """
+    build structure
+    tags = {'amenity': ['school', 'university', '...'], 'leisure': ['park', ...]}
+    """
+    r = read_yaml('tags.yaml')
+    if t_type in {'positive', 'negative', 'education'}:
+        return r[t_type]
 
-    # dfs_ = load_from_pkls() # second
-    # print(dfs_)
+
+def get_tags_test():
+    print('Positive: ', get_tags('positive'))
+    print('Negative: ', get_tags('negative'))
+    print('Education: ', get_tags('education'))
+
+
+def get_df_by(df, tags: Dict):
+    tags = set(chain(*tags.values()))  # flatten
+    return df[df['type_'].isin(tags)]
+
+
+def calc_first_condition():
+    """
+    Для районов: сумма положительных точек в муниципалитете (районе
+    города) превышает отрицательные на 50%
+    weight = 0.6
+    """
+
+    dfs_ = load_from_pkls()  # second
+
+    # TODO: working on nan values
+
+    lst = []
+
+    for distr, df in dfs_.items():
+        df: pd.DataFrame
+        df_education = get_df_by(df, get_tags('education'))
+        df_positive = get_df_by(df, get_tags('positive'))
+        df_negative = get_df_by(df, get_tags('negative'))
+
+        lst.append([distr, len(df_education), len(df_positive), len(df_negative)])
+
+    result_df = pd.DataFrame(lst, columns=['Район', 'Образовательные', 'Положительные', 'Отрицательные'])
+    result_df.set_index('Район', inplace=True)
+    result_df['Cond(pos > 1.5 * neg)'] = result_df['Положительные'] > 1.5 * result_df['Отрицательные']
+    print(result_df)
+
+    return result_df
+
+
+def calc_second_condition():
+    """
+       Для районов: число точек продажи алкоголя или табака или фастфуда
+        удаленность от образовательных учреждений, у которых менее 100м в
+        районе больше или равно 1
+    """
+    dfs_ = load_from_pkls()  # second
+
+    lst = []
+    for distr, df in dfs_.items():
+        df: pd.DataFrame
+        df_education = get_df_by(df, get_tags('education'))
+        df_positive = get_df_by(df, get_tags('positive'))
+        df_negative = get_df_by(df, get_tags('negative'))
+
+        # print(df_education)
+
+        df_negative['center'] = df_negative['geometry'].apply(lambda a: a.centroid)
+        points_negative = df_negative['center'].tolist()
+
+        df_education['center'] = df_education['geometry'].apply(lambda a: a.centroid)
+
+        for k in df_education[['name', 'center']].itertuples():
+            education_point = k.center
+            radius_meters = 150 #
+
+            points_inside_circle = []
+            for point in points_negative:
+                distance = geodesic((education_point.y, education_point.x), (point.y, point.x)).meters
+                if distance <= radius_meters:
+                    points_inside_circle.append(point)
+            if len(points_inside_circle) > 0:
+                lst.append((distr, k.name, len(points_inside_circle)))
+    result_df = pd.DataFrame(lst, columns=['Район', 'Образовательное уч.', 'Neg points'])
+    result_df.set_index('Район', inplace=True)
+    print(result_df)
+    return result_df
+
+
+if __name__ == '__main__':
+    calc_first_condition()
+    calc_second_condition()
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 import pandas as pd
 import yaml
@@ -10,10 +11,20 @@ def read_yaml(file):
         return yaml.load(f, Loader=yaml.SafeLoader)
 
 
-
 def to_pkl(df, name):
     df.to_pickle(f"data/{name}.pkl")
 
 
 def load_pkl(name):
     return pd.read_pickle(f"data/{name}.pkl")
+
+
+def to_json(name, data):
+    with open(f"data/{name}.json", 'w') as f:
+        json.dump(data, f)
+
+
+def load_json(name):
+    print(name)
+    with open(f"data/{name}.json", 'r') as f:
+        return json.load(f)
